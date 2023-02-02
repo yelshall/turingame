4c0e01ccd690d8389444b4966a4cbe949120498e
yelshall <youssufshall@gmail.com>
2023-02-02 17:50:21 +0400

    add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/provider'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	<ChakraProvider>
+		return <Component {...pageProps} />
+	</ChakraProvider>
 }
