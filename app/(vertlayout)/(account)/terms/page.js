d2f5b43da39010d7c19a4e53e9f52f549e6a87ad
Abigail Finney <finney2@purdue.edu>
2023-02-19 20:38:24 -0500

    added TOS text to terms page

--- app/(vertlayout)/(account)/terms/page.js
+++ app/(vertlayout)/(account)/terms/page.js
@@ -7,6 +7,7 @@ import {
 	SkeletonText,
 	Box,
 	Checkbox,
+	Text,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
 
@@ -25,8 +26,19 @@ export default function Page() {
 			<Stack
 				justifyContent='center'
 				alignItems={'center'}>
-				<Box p='16px' w='400px'>
-					<SkeletonText noOfLines={13} spacing='4' skeletonHeight='2' />
+				<Box p='16px' w='500px'>
+						<Text fontSize='md'>Our aim with turingame.ai is to create a novel way for users to engage both with emerging AI technologies and other players. We have created a set of guidelines for user conduct when sending messages to ensure that turingame.ai remains an enjoyable experience for our users.</Text>
+                       	<br />
+						<Text as='b' fontSize='lg'>Be Respectful</Text>
+                       	<Text fontSize='md'>We want to keep the turingame.ai experience fun and inviting for all. That is why we do not allow your in-game messages to contain explicit, harassing, or offensive content of any kind. We do not allow swearing, hateful conduct, harassment, or trolling.</Text>
+                       	<br />
+						<Text as='b' fontSize='lg'>Be Safe</Text>
+                       	<Text fontSize='md'>We want our users to protect their personal security while using our service. We do not allow users to share personally identifying information such as full name or exact location.</Text>
+                       	<br />
+						<Text as='b' fontSize='lg'>No Spam or Advertising</Text>
+                       	<Text fontSize='md'>We want users to be able to enjoy an engaging experience free from advertisements and irrelevant spam messages. We do not allow users to send advertising messages, links to outside sources, or repetitive spam.</Text>
+						<br />
+                       	<Text fontSize='md'>Violating these community guidelines may result in account suspension or deletion.</Text>
 				</Box>
 				<Checkbox>I agree to the terms of service.</Checkbox>
 				<Button onClick={() => {router.push('/')}} w='300px' bg='#2A4365'>Create account</Button>
