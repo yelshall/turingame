b23bc5016aab4ea38a4c81538e1c9bad97fe1d07
yelshall <youssufshall@gmail.com>
2023-02-21 14:50:40 +0400

    adjust ask page

--- app/(vertlayout)/(game)/ask/page.js
+++ app/(vertlayout)/(game)/ask/page.js
@@ -1,96 +1,63 @@
 'use client';
 
 import {
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-  Button,
-  Heading,
-  Text,
-  Divider,
-  Box,
-  InputRightElement,
-  InputGroup,
-  CardBody,
-  Card,
-  CardHeader,
-  CardFooter,
+	Button,
+	Heading,
+	Box,
+	Stack,
+	Input
 } from '@chakra-ui/react';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
+const QuestionItem = ({ text, onChange }) => {
+	return (
+		<Box
+			display='flex'
+			flexDir={'column'}
+			w='100%'
+			pb="16px"
+		>
+			<Heading fontSize={"2xl"} alignSelf={"flex-start"} pb="16px">{text}</Heading>
+			<Input
+				placeholder={"Type your question..."}
+				onChange={onChange}
+				w='500px'
+			/>
+		</Box>
+	)
+};
 export default function Page() {
-  const [question1, setAnswer1] = useState('');
-  const [question2, setAnswer2] = useState('');
-  const [question3, setAnswer3] = useState('');
+	const router = useRouter();
 
-  useEffect(() => {
-    console.log(question1, question2, question3);
-  }, [question1, question2, question3]);
+	const [question1, setAnswer1] = useState('');
+	const [question2, setAnswer2] = useState('');
+	const [question3, setAnswer3] = useState('');
 
-  return (
-    <Box
-      display='flex'
-      flexDir={'column'}
-      w='100%'
-      h='100%'
-      justifyContent='center'
-      alignItems={'center'}
-    >
-      <Heading fontSize={'4xl'} pb='16px'>
-        Ask your 3 questions
-      </Heading>
-      <Card>
-        <Card>
-          <CardBody>
-            <FormLabel>Question 1:</FormLabel>
-            <FormControl pb='8px'>
-              <Input
-                value={question1}
-                onChange={(event) => setAnswer1(event.target.value)}
-                placeholder='Type your question'
-                w='800px'
-                type='question1'
-              />
-            </FormControl>
-          </CardBody>
-        </Card>
-        <Divider color={'#2B6CB0'} />
-        <Card>
-          <CardBody>
-            <FormControl pb='8px'>
-              <FormLabel>Question 2</FormLabel>
-              <Input
-                value={question2}
-                onChange={(event) => setAnswer2(event.target.value)}
-                placeholder='Type your question'
-                w='800px'
-                type='question2'
-              />
-            </FormControl>
-          </CardBody>
-        </Card>
-        <Divider color={'#2B6CB0'} />
-        <Card>
-          <CardBody>
-            <FormControl pb='8px'>
-              <FormLabel>Question 3</FormLabel>
-              <Input
-                value={question3}
-                onChange={(event) => setAnswer3(event.target.value)}
-                placeholder='Type your question'
-                w='800px'
-                type='question3'
-              />
-            </FormControl>
-          </CardBody>
-        </Card>
-        <Divider color={'#2B6CB0'} />
-        <Button variant='ghost' colorScheme='blue' color={'#2B6CB0'}>
-          Submit your questions
-        </Button>
-      </Card>
-    </Box>
-  );
+	const questions = [
+		{ text: "Question 1", setAnswer: setAnswer1 },
+		{ text: "Question 2", setAnswer: setAnswer2 },
+		{ text: "Question 3", setAnswer: setAnswer3 },
+	]
+
+	return (
+		<Box
+			display='flex'
+			flexDir={'column'}
+			w='100%'
+			h='100%'
+			justifyContent='center'
+			alignItems={'center'}
+		>
+			<Heading fontSize={'4xl'} pb='16px'>
+				Ask your 3 questions
+			</Heading>
+			<Stack>
+				{questions.map((question) => (
+					<QuestionItem text={question.text} onChange={(event) => { question.setAnswer(event.target.value) }} />
+				))}
+				<Button bg='#2A4365' onClick={() => { router.push('/answer') }}>Submit</Button>
+			</Stack>
+		</Box>
+	);
 }
